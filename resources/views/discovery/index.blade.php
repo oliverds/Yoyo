9b244847c64fb1d2c386a090e33252c111dbb60a
Oliver Servín <oliverds@users.noreply.github.com>
2023-05-06 21:46:51 -0600

    wip

--- resources/views/discovery/index.blade.php
+++ resources/views/discovery/index.blade.php
@@ -32,36 +32,160 @@
                         <p class="text-neutral-500 text-sm">Framer Resources</p>
                     </div>
                     <div>
-                        <a href="#" class="inline-block text-neutral-500 border border-neutral-300 p-3 rounded-md">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                        <a href="#" class="inline-block border border-neutral-300 py-2.5 px-6 rounded-md text-sm font-semibold text-neutral-900">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-neutral-500 hidden">
                                 <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clip-rule="evenodd" />
                                 <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clip-rule="evenodd" />
                             </svg>
+
+                            View
                         </a>
                     </div>
                 </div>
 
                 <div class="p-4 flex gap-5 items-center border-neutral-300 border rounded-md bg-neutral-100">
                     <div>
-                        <img class="object-cover h-12 w-12 rounded-md" src="https://framerusercontent.com/images/hDQUaWouoxyIThqey4W06r9HIM.webp" alt="">
+                        <img class="object-cover h-12 w-12 rounded-md" src="https://framerusercontent.com/images/vftCZbEK2ive6knUUTRekPAGwj8.webp" alt="">
                     </div>
                     <div class="flex-1">
-                        <h3 class="text-sm font-semibold text-neutral-900">Canvas Supply</h3>
-                        <p class="text-neutral-500 text-sm">Framer Resources</p>
+                        <h3 class="text-sm font-semibold text-neutral-900">DashFolio</h3>
+                        <p class="text-neutral-500 text-sm">The original dashboard-styled Framer Template.</p>
                     </div>
                     <div>
-                        <a href="#" class="inline-block text-neutral-500 border border-neutral-300 p-3 rounded-md">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                        <a href="#" class="inline-block border border-neutral-300 py-2.5 px-6 rounded-md text-sm font-semibold text-neutral-900">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-neutral-500 hidden">
                                 <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clip-rule="evenodd" />
                                 <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clip-rule="evenodd" />
+                            </svg>
+
+                            Buy
+                        </a>
+                    </div>
+                </div>
+
+                <div class="p-4 flex gap-5 items-center border-neutral-300 border rounded-md bg-neutral-100">
+                    <div>
+                        <img class="object-cover h-12 w-12 rounded-md" src="https://framerusercontent.com/images/fxcaCZXYS9IfOcU9diSBiIGo84.webp" alt="">
+                    </div>
+                    <div class="flex-1">
+                        <h3 class="text-sm font-semibold text-neutral-900">Godlike</h3>
+                        <p class="text-neutral-500 text-sm">Build Websites at Godlike Speed.</p>
+                    </div>
+                    <div>
+                        <a href="#" class="inline-block border border-neutral-300 py-2.5 px-6 rounded-md text-sm font-semibold text-neutral-900">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-neutral-500 hidden">
+                                <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clip-rule="evenodd" />
+                                <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clip-rule="evenodd" />
+                            </svg>
+
+                            View
+                        </a>
+                    </div>
+                </div>
+
+                <div class="p-4 flex gap-5 items-center justify-between border-neutral-300 border rounded-md bg-neutral-100">
+                    <div>
+                        <h3 class="text-sm font-semibold text-neutral-900">
+                            Latest Video
+                        </h3>
+                    </div>
+                    <div>
+                        <a href="#" class="inline-flex items-center text-sm text-neutral-500">
+                            Youtube
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 ml-1">
+                                <path fill-rule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clip-rule="evenodd" />
                             </svg>
                         </a>
                     </div>
                 </div>
+
+                <div class="p-4 flex gap-5 items-center justify-between border-neutral-300 border rounded-md bg-neutral-100">
+                    <div>
+                        <h3 class="text-sm font-semibold text-neutral-900">
+                            Gallery
+                        </h3>
+                    </div>
+                    <div>
+                        <a href="#" class="inline-flex items-center text-sm text-neutral-500">
+                            View more
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 ml-1">
+                                <path fill-rule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="p-4 flex gap-5 items-center justify-between border-neutral-300 border rounded-md bg-neutral-100">
+                    <div>
+                        <h3 class="text-sm font-semibold text-neutral-900">
+                            New Podcast Episode
+                        </h3>
+                    </div>
+                    <div>
+                        <a href="#" class="inline-flex items-center text-sm text-neutral-500">
+                            All
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 ml-1">
+                                <path fill-rule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="flex w-full justify-between gap-4">
+                    <div class="w-full flex justify-center border border-neutral-300 bg-neutral-100 p-3 rounded-md">
+                        <a class="h-[80px] flex items-center" href="#">
+                            twitter
+                        </a>
+                    </div>
+                    <div class="w-full flex justify-center border border-neutral-300 bg-neutral-100 p-3 rounded-md">
+                        <a class="h-[80px] flex items-center" href="#">
+                            youtube
+                        </a>
+                    </div>
+                    <div class="w-full flex justify-center border border-neutral-300 bg-neutral-100 p-3 rounded-md">
+                        <a class="h-[80px] flex items-center" href="#">
+                            instagram
+                        </a>
+                    </div>
+                    <div class="w-full flex justify-center border border-neutral-300 bg-neutral-100 p-3 rounded-md">
+                        <a class="h-[80px] flex items-center" href="#">
+                            facebook
+                        </a>
+                    </div>
+                    <div class="w-full flex justify-center border border-neutral-300 bg-neutral-100 p-3 rounded-md">
+                        <a class="h-[80px] flex items-center" href="#">
+                            phone
+                        </a>
+                    </div>
+                </div>
+
+                <div class="p-4 flex gap-5 items-center justify-between border-neutral-300 border rounded-md bg-neutral-100">
+                    <div>
+                        Newsletter form
+                    </div>
+                </div>
+
+                <div class="p-4 flex gap-5 items-center justify-between border-neutral-300 border rounded-md bg-neutral-100">
+                    <div>
+                        <h3 class="text-sm font-semibold text-neutral-900">
+                            Stack
+                        </h3>
+                    </div>
+                </div>
             </div>
 
-            <div>
-                footer
+            <div class="flex gap-4 text-sm">
+                <div class="flex gap-1">
+                    <span>Proundly built in</span>
+                    <a href="#" class="text-neutral-500 hover:text-neutral-900">Framer</a>
+                </div>
+                <div class="flex gap-1">
+                    <span>Made by</span>
+                    <a href="#" class="text-neutral-500 hover:text-neutral-900">Cedric</a>
+                </div>
+                <div class="flex gap-1">
+                    <span class="text-neutral-500">©2023 Linxs Template</span>
+                </div>
             </div>
         </div>
     </main>
