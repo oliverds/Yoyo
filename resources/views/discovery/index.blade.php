245e7b6641157692ec4520d9e487e202c4ec5a1f
Oliver Servín <oliverds@users.noreply.github.com>
2023-05-06 21:08:25 -0600

    wip

--- resources/views/discovery/index.blade.php
+++ resources/views/discovery/index.blade.php
@@ -1,29 +1,68 @@
 <x-app-layout>
-    <div class="flex flex-col justify-center items-center px-2 min-h-screen text-neutral-900">
-        <main class="flex flex-col flex-1 justify-center items-center py-20 px-0">
-            <h1 class="mb-4 text-6xl font-bold text-center">
-                Welcome to
-                <span class="text-purple-600">
-                    Enhaced Laravel!
-                </span>
-            </h1>
+    <main class="flex flex-col items-center gap-16 pt-24 pb-40 md:py-30">
+        <div class="flex flex-col gap-16 items-center px-8 w-full max-w-screen-sm md:gap-12 md:px-6">
+            <header class="flex flex-col gap-6 items-center px-3 w-full">
+                <a href="/" class="flex">
+                    <img class="rounded-full h-24" src="https://framerusercontent.com/images/lFcgmnSNMxgS8b8a78iKsMuURd0.webp">
+                </a>
+                <h1 class="text-4xl font-semibold tracking-tight text-neutral-900 max-w-sm">
+                    Chris Linx
+                </h1>
+                <h2 class="text-center max-w-sm">
+                    Experienced UX designer based in the US known for creating intuitive and
+                    engaging user experiences.
+                </h2>
+                <div class="flex justify-center gap-4">
+                    <a href="#" class="border border-neutral-300 rounded py-3 px-6 bg-neutral-100 text-sm font-semibold text-neutral-900">
+                        Website
+                    </a>
+                    <a href="#" class="border border-neutral-300 rounded py-3 px-6 text-sm font-semibold text-neutral-900">
+                        Email
+                    </a>
+                </div>
+            </header>
 
-            <div class="my-6">
-                <p class="text-2xl leading-normal text-center">
-                    The source for this page lives in
-                    <code class="p-3 font-mono text-lg leading-7 rounded break-all bg-neutral-50">
-                        resources/views/discovery
-                    </code>
-                </p>
+            <div class="w-full flex flex-col gap-4">
+                <div class="p-4 flex gap-5 items-center border-neutral-300 border rounded-md bg-neutral-100">
+                    <div>
+                        <img class="object-cover h-12 w-12 rounded-md" src="https://framerusercontent.com/images/hDQUaWouoxyIThqey4W06r9HIM.webp" alt="">
+                    </div>
+                    <div class="flex-1">
+                        <h3 class="text-sm font-semibold text-neutral-900">Canvas Supply</h3>
+                        <p class="text-neutral-500 text-sm">Framer Resources</p>
+                    </div>
+                    <div>
+                        <a href="#" class="inline-block text-neutral-500 border border-neutral-300 p-3 rounded-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clip-rule="evenodd" />
+                                <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
 
-                <p class="text-2xl leading-normal text-center mt-4">
-                    Edit
-                    <code class="p-3 font-mono text-lg leading-7 rounded break-all bg-neutral-50">
-                        resources/views/discovery/index.blade.php
-                    </code>
-                    to see your changes
-                </p>
+                <div class="p-4 flex gap-5 items-center border-neutral-300 border rounded-md bg-neutral-100">
+                    <div>
+                        <img class="object-cover h-12 w-12 rounded-md" src="https://framerusercontent.com/images/hDQUaWouoxyIThqey4W06r9HIM.webp" alt="">
+                    </div>
+                    <div class="flex-1">
+                        <h3 class="text-sm font-semibold text-neutral-900">Canvas Supply</h3>
+                        <p class="text-neutral-500 text-sm">Framer Resources</p>
+                    </div>
+                    <div>
+                        <a href="#" class="inline-block text-neutral-500 border border-neutral-300 p-3 rounded-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clip-rule="evenodd" />
+                                <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
             </div>
-        </main>
-    </div>
+
+            <div>
+                footer
+            </div>
+        </div>
+    </main>
 </x-app-layout>
